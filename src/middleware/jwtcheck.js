7ab618b4c9e4063489fbdcc0cc2cc33effb29f84
Mahammad Munzir <munzircnr710@gmail.com>
2024-05-26 21:25:03 +0530

    first commit

--- src/middleware/jwtcheck.js
+++ src/middleware/jwtcheck.js
@@ -4,7 +4,7 @@ export const authenticateUser = (req, res, next) => {
      const token = req.cookies.access_token;
      
      if (!token) {
-        return res.status(401).json({error: "Unauthorized"});
+        return res.status(401).json({error: "Unauthorized", token});
       }
 
       jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
